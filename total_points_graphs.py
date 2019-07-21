5a32ebf2c7a6bee4511d747bc9d5a7529332b659
Tamieem <tamieemjaffary@gmail.com>
2019-07-21 03:59:11 -0400

    Actually fix main, with running program Add new WIP chart: "total points" line graph of a single game

--- total_points_graphs.py
+++ total_points_graphs.py
@@ -1,5 +1,7 @@
 from nba_api.stats.endpoints import shotchartdetail
 from nba_api.stats.endpoints import playbyplayv2
+from nba_api.stats.endpoints import scoreboard
+from datetime import date, datetime, timedelta
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -11,14 +13,21 @@ def get_games(game_id):
 ### Important Columns to note:  [4]=Quarter [6]=time left in quarter [10]= current score [11]= score margin (home team is postive)
 
     gameplay = playbyplayv2.PlayByPlayV2(game_id)
-    gameplay.play_by_play.get_dict() ## [10] holds score, [11] holds score margin
+    pbp = gameplay.play_by_play.get_dict() ## [10] holds score, [11] holds score margin
 
-    play_by_plays = []
-    for link_set in link_sets:
-        for link in link_set:
-            href = link.get("href")
-            if "playbyplay" in href:
-                play_by_plays.append(href)
 
-    return play_by_plays
+
+if date.today() <= datetime.strptime("2019-04-30", "%Y-%m-%d").date():
+    date = datetime.now().strftime("%Y-%m-%d")
+else:
+    date = "2019-04-30"
+schedule = scoreboard.Scoreboard(day_offset='0', game_date=date, league_id='00').get_dict()['resultSets'][0]
+## Will need to include a calendar option that will == schedule variable
+# (with dropdown list of games refreshing on each on click action)
+
+ #or sched in schedule:  ## Will need to produce a dropdown list of options of games that had happened within that day
+    #print(sched)
+game_id = schedule['rowSet'][0][2]
+print("test")
+get_games(game_id)
 
