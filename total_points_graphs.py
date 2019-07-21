faa7886b3bbfd3093ac44436b2536e6d8b4e60c0
Tamieem <tamieemjaffary@gmail.com>
2019-07-21 17:15:58 -0400

    Create layout for a singular game score margin line chart Only displays 1st quarter Need to format graph to display all quarters in one graph but separate lines Need to add titles and text to chart Need to produce tooltips on hover for each plot (already have labels for each)

--- total_points_graphs.py
+++ total_points_graphs.py
@@ -5,16 +5,40 @@ from datetime import date, datetime, timedelta
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib import dates
 from matplotlib.patches import Circle, Rectangle, Arc
 from matplotlib.offsetbox import OffsetImage
 
-
-def get_games(game_id):
-### Important Columns to note:  [4]=Quarter [6]=time left in quarter [10]= current score [11]= score margin (home team is postive)
-
+fig = plt.figure()
+plt.ion()
+def get_game(game_id):
+### Important Columns to note:  [4]=Quarter [6]=time left in quarter [10]= current score  (AWAY_SCORE - HOME_SCORE) [11]= score margin (home team is postive)
+    x_data = [[], [], [], []]
+    y_data = [[], [], [], []]
+    label_data = [[], [], [], []]
     gameplay = playbyplayv2.PlayByPlayV2(game_id)
-    pbp = gameplay.play_by_play.get_dict() ## [10] holds score, [11] holds score margin
+    plays = gameplay.play_by_play.get_dict()['data'] ## [10] holds score, [11] holds score margin
+    quarter = 0
 
+    for play in plays:
+        if play[11] is not None and play[6] != '0:00' and play[6] != '12:00':
+            if quarter != int(play[4])-1:
+                quarter = int(play[4]) - 1
+            time = dates.date2num(datetime.strptime(play[6], '%M:%S'))
+            time = (quarter * 12)+ time
+            x_data[quarter].append(time)
+            if play[11] == 'TIE':
+                y_data[quarter].append(0)
+            else:
+                y_data[quarter].append(int(play[11]))
+            if play[7] is None: # play[7] = Home team description
+                label_data[quarter].append('' + play[9] + '\nScore: ' + play[10]) # play[9] = Away team description
+            else:
+                label_data[quarter].append('' + play[7] + '\nScore: ' + play[10])
+    plt.plot(x_data[0], y_data[0], 'ro-')
+    plt.xlim(dates.date2num(datetime.strptime('12:00', '%M:%S')), dates.date2num(datetime.strptime('00:00', '%M:%S')))
+    plt.gca().xaxis.set_major_formatter(dates.DateFormatter('%M:%S'))
+    plt.show()
 
 
 if date.today() <= datetime.strptime("2019-04-30", "%Y-%m-%d").date():
@@ -29,5 +53,5 @@ schedule = scoreboard.Scoreboard(day_offset='0', game_date=date, league_id='00')
     #print(sched)
 game_id = schedule['rowSet'][0][2]
 print("test")
-get_games(game_id)
+get_game(game_id)
 
